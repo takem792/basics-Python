cd9637262cf04bfd109efa2072361ce5b8416e86
takem792 <takem@yandex.ru>
2019-04-10 17:40:27 +0300

    rwef

--- lesson5-easy-task1.py
+++ lesson5-easy-task1.py
@@ -4,27 +4,37 @@
 # И второй скрипт, удаляющий эти папки.
 
 
-import os
-dir_name = 'dir_'
-# делаем папки
-dir_create = []
-for num in range(1, 3):
-    try:
-        os.mkdir(dir_name+str(num))
-        dir_create.append(dir_name+str(num))
-    except FileExistsError:
-        print('Такая папка уже существует')
-print("Созданы следующие папки:", dir_create)
-# удаляем папки
-des = input('удаляем? (y/n)\n')
-dir_remove = []
-if des == "y":
+def cr_dir():
+    import os
+    dir_name = 'dir_'
+    # делаем папки
+    dir_create = []
     for num in range(1, 3):
         try:
-            os.rmdir(dir_name+str(num))
-            dir_remove.append(dir_name+str(num))
-        except FileNotFoundError:
+            os.mkdir(dir_name+str(num))
+            dir_create.append(dir_name+str(num))
+        except FileExistsError:
             pass
-    print('Удалены следующие папки:', dir_remove)
-else:
-    print("Как знаешь, но этот мусор кому-то удалять придется... :)")
+    print("Созданы следующие папки:", dir_create)
+
+
+def rem_dir():
+    import os
+    dir_name = 'dir_'
+    # удаляем папки
+    des = input('удаляем? (y/n)\n')
+    dir_remove = []
+    if des == "y":
+        for num in range(1, 3):
+            try:
+                os.rmdir(dir_name+str(num))
+                dir_remove.append(dir_name+str(num))
+            except FileNotFoundError:
+                pass
+        print('Удалены следующие папки:', dir_remove)
+    else:
+        print("Как знаешь, но этот мусор кому-то удалять придется... :)")
+
+
+cr_dir()
+rem_dir()
